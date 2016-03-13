b46c3084248ad0187d407f4a62264df5aa05dc7d
Tim <tduncan72@gmail.com>
2016-03-13 11:16:43 -0400

    Add class for containing error data

---
+++ TOTD.Errors.Core/Error.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TOTD.Errors.Core
+{
+    public class Error : Error<int>
+    {
+    }
+
+    public class Error<TKey>
+    {
+        public TKey ID
+        {
+            get;
+            set;
+        }
+
+        public DateTimeOffset WhenOccurred
+        {
+            get;
+            set;
+        }
+
+        public string ErrorType
+        {
+            get;
+            set;
+        }
+
+        public string ErrorSource
+        {
+            get;
+            set;
+        }
+
+        public string ErrorMessage
+        {
+            get;
+            set;
+        }
+
+        public string StackTrace
+        {
+            get;
+            set;
+        }
+
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        protected object Detail
+        {
+            get;
+            set;
+        }
+    }
+}
