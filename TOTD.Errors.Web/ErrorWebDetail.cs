05804cf68d9dbc73bdec075b53f40850e3edae5f
Tim <tduncan72@gmail.com>
2016-03-13 11:23:22 -0400

    Add project for logging errors from web apps

---
+++ TOTD.Errors.Web/ErrorWebDetail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TOTD.Errors.Web
+{
+    public class ErrorWebDetail
+    {
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        public IReadOnlyDictionary<string, string[]> QueryString
+        {
+            get;
+            private set;
+        }
+
+        public IReadOnlyDictionary<string, string[]> FormValues
+        {
+            get;
+            private set;
+        }
+
+        public IReadOnlyDictionary<string, string[]> Cookies
+        {
+            get;
+            private set;
+        }
+
+        public IReadOnlyDictionary<string, string[]> ServerVariables
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ TOTD.Errors.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TOTD.Errors.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("TOTD.Errors.Web")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ae0d9e5-5ab8-4ee7-aad5-db21a4c005d7")]
